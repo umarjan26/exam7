92e82c41505feba8e5582223906c1785ef06b8c4
umar <umarjan260797@gmail.com>
2022-07-30 11:48:16 +0300

    создали модели poll, choice

---
+++ source/webapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=300, null=False, blank=False, verbose_name='Название Опроса')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания опроса')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=3000,null=False, blank=False, verbose_name='Текст варианта' )
+    poll = models.ForeignKey('webapp.Poll', related_name='choice', null=True, blank=False,
+                                on_delete=models.CASCADE, verbose_name='Опрос')
+
