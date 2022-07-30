ec0c31b4e2399456b7dd0452c263223c9fa33fc6
umar <umarjan260797@gmail.com>
2022-07-30 13:38:39 +0300

    сделали  страницу детального просмотра

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views.views import IndexView
+from webapp.views.views import IndexView, DetailsView
 
 urlpatterns = [
-    path ('', IndexView.as_view(), name="poll")
+    path ('', IndexView.as_view(), name="poll"),
+    path ('poll/<int:pk>/', DetailsView.as_view(), name="detail")
 ]
--- source/webapp/views/views.py
+++ source/webapp/views/views.py
@@ -1,6 +1,6 @@
 from webapp.models import Poll
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class IndexView(ListView):
@@ -10,3 +10,12 @@ class IndexView(ListView):
     ordering = ("-created_at",)
     paginate_by = 5
     paginate_orphans = 0
+
+    def get_queryset(self):
+        return Poll.objects.all().order_by('-created_at')
+
+
+class DetailsView(DetailView):
+    template_name = 'poll/detail_view.html'
+    model = Poll
+    context_object_name = 'poll'
